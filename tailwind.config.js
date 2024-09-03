e4a8b0301529489d159a14ea8596853d2973ca52
Reinier Hernández <sasuke.reinier@gmail.com>
2024-09-03 17:27:47 -0300

    feat: added suri information and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4CAF50',
+        secondary: '#E0F7FA',
+        accent: '#FFC107',
+        card: '#F5F5F5',
+        'card-foreground': '#212121',
+      },
+    },
   },
   plugins: [],
 }
