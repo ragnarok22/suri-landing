8163ad66fe99a122c2b1b233e23f1ace65248e0a
Reinier Hernández <sasuke.reinier@gmail.com>
2024-09-03 22:03:09 -0300

    feat: added first version of suri apk

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), tailwind()],
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
-  integrations: [react(), tailwind()],
 });
