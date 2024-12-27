4648f1ca0fd3108e08920151c97ff72ab762a82d
Reinier Hernández <sasuke.reinier@gmail.com>
2024-12-27 15:00:40 -0300

    feat(api): enhance telesur endpoint with prepaid options

--- src/pages/api/telesur.json.ts
+++ src/pages/api/telesur.json.ts
@@ -1,5 +1,55 @@
 import type { APIRoute } from "astro";
 
+type TelesurOptionsType = {
+  call: {
+    CHECK_BALANCE: string;
+    RECHARGE_BALANCE: string;
+    ACTIVATE_PRIVATE_MODE: string;
+    DEACTIVATE_PRIVATE_MODE: string;
+  };
+  sms: {
+    CHECK_MOBILE_DATA: {
+      number: string;
+      code: string;
+    };
+    SET_P2P_PIN: {
+      number: string;
+      code: string;
+    };
+    TRANSFER_BALANCE: {
+      number: string;
+      code: string;
+    };
+  };
+};
+
 export const GET: APIRoute = ({ params, request }) => {
-  return new Response('{"telesur": "telesur"}');
+  const prepaid: TelesurOptionsType = {
+    call: {
+      CHECK_BALANCE: "*132#",
+      RECHARGE_BALANCE: "*131*{pincode}#",
+      ACTIVATE_PRIVATE_MODE: "*31#",
+      DEACTIVATE_PRIVATE_MODE: "#31#",
+    },
+    sms: {
+      CHECK_MOBILE_DATA: {
+        number: "4040",
+        code: "NET INFO",
+      },
+      SET_P2P_PIN: {
+        number: "134",
+        code: "SET {pin}",
+      },
+      TRANSFER_BALANCE: {
+        number: "134",
+        code: "p2p {pincode} {amount} {phone_number}",
+      },
+    },
+  } as const;
+
+  const telesur = {
+    prepaid,
+  };
+
+  return new Response(JSON.stringify(telesur));
 }
